17d9b6dfe6d8ea5087c9f20ef679bfb436827048
Sean Hong(홍성민) <seanhong2000@gmail.com>
2022-06-12 15:46:46 +0900

    Initial Commit

---
+++ fdrone/fdroneApp.swift
@@ -0,0 +1,17 @@
+//
+//  fdroneApp.swift
+//  fdrone
+//
+//  Created by Sean Hong on 2022/06/12.
+//
+
+import SwiftUI
+
+@main
+struct fdroneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
